a3caad5264e7d3c539568df02e2cd3f39c2c625a
0xBooper <77999320+0xBooper@users.noreply.github.com>
2021-07-13 22:39:29 -0500

    Made the RandomBool(randomness: number) function dynamic.

--- src/Helpers/randomBool.js
+++ src/Helpers/randomBool.js
@@ -1,3 +1,12 @@
-export default function RandomBool() {
-    return Math.random() < 0.5
+export default function RandomBool(randomness) {
+    let randomnessVar = randomness < 10 ? randomness / 10 : null // If randomness is single-digit, make it a decimal. Otherwise, make it NULL
+
+    // If randomness var is NULL, error and return early
+    if (!randomnessVar) {
+        console.error("Randommness Parameter must be single digit.")
+        return
+    }
+
+    return Math.random() < randomnessVar // Return true/false if randomnessVar is greater than Math.random
+
 }
